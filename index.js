0f01df4db1aaf121fbb737b352dbb10e4f630885
Werefox <jack@wrightus.com>
2022-01-26 18:52:25 -0500

    Inital commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let header = document.querySelector("#page-header")
+header.style.textAlign = "right"
 
-// Select some elements...
+let dogImages = document.querySelectorAll(".dog-image")
+
+for (let i = 0; i < dogImages.length; i++) 
+{
+	let radius = Math.random() * 100
+	dogImages[i].style.borderRadius = radius + "px"
+	let rotation = Math.random() * 360
+	dogImages[i].style.transform = "rotate("+ rotation +"deg)"
+}
+
+let dogNames = document.querySelectorAll(".dog-name")
+
+for (let i = 0; i < dogNames.length; i++) 
+{
+	dogNames[i].style.textAlign = "left"
+}
